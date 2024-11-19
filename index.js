3389228e0ef483ee0f19ad6520c6c2b91140320d
Sagar Radadiya <sagar.mivaninfotech@gmail.com>
2024-11-19 18:19:08 +0530

    create middleware file

--- index.js
+++ index.js
@@ -31,7 +31,76 @@ console.log("sdfsdfsfg");
 // app.listen(500)
 
 
-const express=require('express')
+
+// //  middlewar
+// const express = require('express')
+// const app = express()
+
+// //  this is aplication middeleware
+// const reqFilter = (req, res, next) => {
+//     if (!req.query.age) {
+//         res.send("add age")
+//     } else if (req.query.age < 18) {
+//         res.send("you can not acces this page")
+//     }
+//     else {                   
+//         next()
+//     }
+// }
+
+// //  this is how middleware apply on all routs
+// app.use(reqFilter)
 
 
+// app.get('/', (req, res) => {
+//     res.send("Welcome to Home page")
+// })
+// app.get('/user', (req, res) => {
+//     res.send("this is user page")
+// })
 
+// app.listen(500)
+
+
+// //  singleroutMiddleware 
+// const reqFilter = require('./middleware')  // middleware define in sepret file
+// const express = require('express')
+
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     res.send("this is home page")
+// })
+
+// // use of routlevel middleware
+// app.get('/about',reqFilter, (req, res) => {
+//     res.send("this is about page")
+// })
+
+// app.listen(550)
+
+
+// //  grouproutemiddleware
+// const express = require('express')
+// const app = express()
+// const router = express.Router()  // create router using epress
+
+// const reqFilter = require('./middleware')
+
+// router.use(reqFilter)   //  apply reqFilter on router 
+
+// app.get('/', (req, res) => {
+//     res.send("this home page")
+// })
+
+// // use router at thew plas of app 
+// router.get('/about', (req, res) => {   
+//     res.send("this about page")
+// })
+
+// router.get('/user', (req, res) => {
+//     res.send("this user page")
+// })
+
+// app.use('/', router) // add router in app
+// app.listen(550)
